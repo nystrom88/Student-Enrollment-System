b6445e40b28e63f7698b7f9d4c2bc6162e9d4baf
Nystrom <philipnystrom88@gmail.com>
2025-03-11 14:15:07 +0100

    added html and css to coursePage

--- src/js/courses/courses.js
+++ src/js/courses/courses.js
@@ -4,3 +4,12 @@ CourseManagement.addCourse("Film");
 CourseManagement.addCourse("TV");
 
 console.log(CourseManagement.coursesList);
+
+// Create/Append Elements
+const listSection = document.createElement("section");
+const list = document.createElement("ul");
+const listInformation = document.createElement("li");
+const listBox = document.createElement("div");
+const instructurName = document.createElement("h3");
+const studentNames = document.createElement("h4");
+const listText = document.createElement("p");
