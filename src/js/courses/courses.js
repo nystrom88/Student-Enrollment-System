08cb41b21eb8be9d47ec0f8129c1d1dedacb90cf
sbraende <sebastian.braende@gmail.com>
2025-03-11 12:46:20 +0100

    Implement base version CourseManagment

--- src/js/courses/courses.js
+++ src/js/courses/courses.js
@@ -1,6 +1,8 @@
 import CourseManagement from "./coursesManagement";
 
-CourseManagement.addCourse("Film");
-CourseManagement.addCourse("TV");
+// CourseManagement.addCourse("Film");
+// CourseManagement.addCourse("TV");
+CourseManagement.editCourse("6bc89698-8903-4765-a8dd-5bb70cd1030a");
+// CourseManagement.removeCourse("04008392-00fc-4d55-a1e1-d77e7fe1db94");
 
-console.log(CourseManagement.coursesList);
+// console.log(CourseManagement.coursesList);
